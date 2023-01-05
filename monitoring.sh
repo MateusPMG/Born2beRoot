285c07ef502cb18812176598d8e77f6a7997f33c
MateusPMG <mpmgboss@gmail.com>
2023-01-05 14:35:58 +0000

    final working

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-echo "Operating system: $(. /etc/os-release; echo "$NAME" "$VERSION")"
-echo "Kernel: $(uname -r)"
-echo "Physical Processors, CPUs: $(nproc)"
-echo "Virtual Processors, VCPUs: $(cat /proc/cpuinfo | grep processor | wc -l)"
-free --mega| grep Mem | awk '{ printf("RAM Usage: %.2f/%.2fMb (%.2f%%)\n", $3, $2, $3/$2 * 100.0) }'
-df -h| grep '^/dev/' | grep -v '/boot$' | awk '{ud += $3} {fd+= $2} END {printf("Total Disk: %.2fGb/%.2fGb (%.2f%)\n", ud, fd, ud/fd*100) }'
-top -bn1 | grep '^%Cpu' | cut -c 9- | awk '{printf("CPU load: %.1f%%\n"), $1+$2+$3}'
-who -b | cut -c 22- | awk '{print("Last reboot: "$1 " " $2)}'
+wall "
+Architecture: $(uname -a)
+Physical Processors, CPUs: $(nproc)
+Virtual Processors, VCPUs: $(cat /proc/cpuinfo | grep processor| wc -l)
+$(free --mega | grep Mem | awk '{ printf("RAM usage: %.2f/%.2fMb (%.2f%%)\n", $3, $2, $3/$2 * 100.0) }')
+$(df -h --total| grep total | awk '{ printf("Total Disk: %.2f/%.2fGb (%.2f%%)\n", $3, $2, $5) }')
+$(top -bn1 | grep '^%Cpu' | cut -c 9- | awk '{printf("CPU load: %.1f%%\n"), $1+$2+$3}')
+$(who -b | cut -c 22- | awk '{print("Last reboot: "$1 " " $2)}')
+Connections TCP: $(ss -tn src :4242 |grep -i "estab" | wc  -l) Established
+User log: $(who | wc -l)
+Network: IP $(hostname -I) ($(ip addr | grep ether | awk '{ print($2) }'))
+Sudo: $(cat /var/log/sudo/sudolog.log | wc -l | awk '{ print $1/2  }') cmd
+$(if grep -q /dev/mapper /etc/fstab
+	then
+echo "LVM in use"
+	else
+echo "LVM not in use"
+	fi)
+	"
